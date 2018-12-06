62c124e586a5da93bded613908b987af17c75a4b
myhrnz1 <myhrnz@outlook.com>
2018-12-06 14:35:21 +0100

    add test start value usernameIsValid passwordIsValid

--- MTTK_Mini_Test.java
+++ MTTK_Mini_Test.java
@@ -1,5 +1,48 @@
 public class MTTK_Mini_Test {
+
+  public static boolean testMTTK_Create_StartValueOftempUsername() {
+    MTTK_Create create = new MTTK_Create();
+    return create.tempUsername.equals("");
+  }
+
+  public static boolean testMTTK_Create_StartValueOftempPassword() {
+    MTTK_Create create = new MTTK_Create();
+    return create.tempPassword.equals("");
+  }
+
+  public static boolean testMTTK_Create_StartValueOfUsernameIsValid() {
+    MTTK_Create create = new MTTK_Create();
+    return create.usernameIsValid == false;
+  }
+
+  public static boolean testMTTK_Create_StartValueOfPasswordIsValid() {
+    MTTK_Create create = new MTTK_Create();
+    return create.passwordIsValid == false;
+  }
+
   public static void main(String[] args) {
-  System.out.println("Test results:");
+    if (testMTTK_Create_StartValueOftempUsername()){
+      System.out.println("PASSED" + ": testMMTK_Create_StartValueOfTempUsernem(): ");
+    } else {
+      System.out.println("FAILED" + ": testMMTK_Create_StartValueOfTempUsernem(): ");
+    }
+
+    if (testMTTK_Create_StartValueOftempPassword()){
+      System.out.println("PASSED" + ": testMTTK_Create_StartValueOftempPassword(): ");
+    } else {
+      System.out.println("FAILED" + ": testMTTK_Create_StartValueOftempPassword(): ");
+    }
+
+    if (testMTTK_Create_StartValueOfUsernameIsValid()){
+      System.out.println("PASSED" + ": testMTTK_Create_StartValueOfUsernameIsValid(): ");
+    } else {
+      System.out.println("FAILED" + ": testMTTK_Create_StartValueOfUsernameIsValid(): ");
+    }
+
+    if (testMTTK_Create_StartValueOfPasswordIsValid()){
+      System.out.println("PASSED" + ": testMTTK_Create_StartValueOfPasswordIsValid(): ");
+    } else {
+      System.out.println("FAILED" + ": testMTTK_Create_StartValueOfPasswordIsValid(): ");
+    }
   }
 }
