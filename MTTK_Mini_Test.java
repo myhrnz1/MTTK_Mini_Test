480163470bfa1d444d77589bda5c5ff19f34671f
myhrnz1 <myhrnz@outlook.com>
2018-12-06 15:54:38 +0100

    added various tests of methods in Create

--- MTTK_Mini_Test.java
+++ MTTK_Mini_Test.java
@@ -20,29 +20,64 @@ public class MTTK_Mini_Test {
     return create.passwordIsValid == false;
   }
 
+  public static boolean testValidUsernameInTryUsername() {
+    MTTK_Create create = new MTTK_Create();
+    return true == create.tryUsername("abc");
+  }
+
+  public static boolean testInValidUsernameInTryUsername() {
+    MTTK_Create create = new MTTK_Create();
+    MTTK_Users.addMTTK_User("abc","123");
+    return false == create.tryUsername("abc");
+  }
+
+  public static boolean testVariablesAfterUsingValidUsernameInTryUsername() {
+    MTTK_Create create = new MTTK_Create();
+    create.tryUsername("abc");
+    return (create.tempUsername == "abc" && create.usernameIsValid == true);
+  }
+
   public static void main(String[] args) {
     if (testMTTK_Create_StartValueOftempUsername()){
-      System.out.println("PASSED" + ": testMMTK_Create_StartValueOfTempUsernem(): ");
+      System.out.println("PASSED" + ": testMMTK_Create_StartValueOfTempUsernem()");
     } else {
-      System.out.println("FAILED" + ": testMMTK_Create_StartValueOfTempUsernem(): ");
+      System.out.println("FAILED" + ": testMMTK_Create_StartValueOfTempUsernem()");
     }
 
     if (testMTTK_Create_StartValueOftempPassword()){
-      System.out.println("PASSED" + ": testMTTK_Create_StartValueOftempPassword(): ");
+      System.out.println("PASSED" + ": testMTTK_Create_StartValueOftempPassword()");
     } else {
-      System.out.println("FAILED" + ": testMTTK_Create_StartValueOftempPassword(): ");
+      System.out.println("FAILED" + ": testMTTK_Create_StartValueOftempPassword()");
     }
 
     if (testMTTK_Create_StartValueOfUsernameIsValid()){
-      System.out.println("PASSED" + ": testMTTK_Create_StartValueOfUsernameIsValid(): ");
+      System.out.println("PASSED" + ": testMTTK_Create_StartValueOfUsernameIsValid()");
     } else {
-      System.out.println("FAILED" + ": testMTTK_Create_StartValueOfUsernameIsValid(): ");
+      System.out.println("FAILED" + ": testMTTK_Create_StartValueOfUsernameIsValid()");
     }
 
     if (testMTTK_Create_StartValueOfPasswordIsValid()){
-      System.out.println("PASSED" + ": testMTTK_Create_StartValueOfPasswordIsValid(): ");
+      System.out.println("PASSED" + ": testMTTK_Create_StartValueOfPasswordIsValid()");
     } else {
-      System.out.println("FAILED" + ": testMTTK_Create_StartValueOfPasswordIsValid(): ");
+      System.out.println("FAILED" + ": testMTTK_Create_StartValueOfPasswordIsValid()");
+    }
+
+    if (testValidUsernameInTryUsername()){
+      System.out.println("PASSED" + ": testValidUsernameInTryUsername()");
+    } else {
+      System.out.println("FAILED" + ": testValidUsernameInTryUsername()");
+    }
+
+    if (testInValidUsernameInTryUsername()){
+      System.out.println("PASSED" + ": testInValidUsernameInTryUsername()");
+    } else {
+      System.out.println("FAILED" + ": testInValidUsernameInTryUsername()");
+    }
+
+    if (testVariablesAfterUsingValidUsernameInTryUsername()){
+      System.out.println("PASSED" + ": testVariablesAfterUsingValidUsernameInTryUsername()");
+    } else {
+      System.out.println("FAILED" + ": testVariablesAfterUsingValidUsernameInTryUsername())");
     }
   }
 }
